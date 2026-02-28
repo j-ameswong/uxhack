fa5477770cd7ed3421f3596f9f81ad1e2194248d
suspectpfx <xiaopengfei@xiaopengfeideMacBook-Pro.local>
2026-02-28 11:30:32 +0000

    feat: created a constants.js file

---
+++ client/src/game/constants.js
@@ -0,0 +1,15 @@
+export const TICK_RATE_MS = 150;
+export const FLEE_RADIUS = 8;
+export const GRID_COLS = 40;
+export const GRID_ROWS = 30;
+export const SCATTER_DELAY_MS = 1500;
+
+export const GAME_CONSTANTS = {
+  TICK_RATE_MS,
+  FLEE_RADIUS,
+  GRID_COLS,
+  GRID_ROWS,
+  SCATTER_DELAY_MS,
+};
+
+export default GAME_CONSTANTS;
